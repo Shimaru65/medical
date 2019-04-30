75a477cb5109bf99e953b17440347e54d7e3cb46
Shimaru65 <47945283+Shimaru65@users.noreply.github.com>
2019-04-30 10:09:18 +0700

    Add files via upload

--- show_result.php
+++ show_result.php
@@ -2,45 +2,58 @@
     <head>   
         <meta charset="windows-874">
     </head>
+    <?php include 'connect/dbconnect.php'; ?>
     <body>
+        <?php
+  
+        ?>
         <div class="content mt-3">
             <div class="animated fadeIn">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card">                           
                             <div class="card-header">
-                                <?php
-                                include 'connect/dbconnect.php';
-                                $showhos1 = "select * from disease_list WHERE disease_id=$_GET[disease_id]";
-                                $result1 = mssql_query($showhos1);
-                                $record = mssql_fetch_array($result1);
-                                ?> 
-                                <strong class="card-title"><?php echo "$record[disease_list_name]" ?></strong>
-                            </div>
-
-                            <div class="card-body">
-                                <table id="bootstrap-data-table" class="table table-striped table-bordered">
-                                    <thead>
-                                        <?php
-                                        include 'connect/dbconnect.php';
-                                        $showhos = "select * from disease_detail WHERE disease_id=$_GET[disease_id]";
-                                        $rusult_table = "select * from result";
-                                        $result = mssql_query($showhos);
-                                        $result3 = mssql_query($rusult_table);
-                                        while ($record = mssql_fetch_array($result)) {
-                                            ?> 
-                                        <th>
+                                <?php 
+                              $show2 = "SELECT dbo.disease_list.disease_list_name, dbo.check_result.year, dbo.check_result.id_employee, dbo.disease_list.disease_id
+FROM            dbo.check_result INNER JOIN
+                         dbo.disease_list ON dbo.check_result.disease_id = dbo.disease_list.disease_id
+WHERE        (dbo.check_result.id_employee = ".$_GET[id_employee]." and check_result.disease_id=".$_GET[disease_id].")";
+                                $result2 = mssql_query($show2);
+                          
+                                while ($record2 = mssql_fetch_array($result2)) {
+                                    
+                                   
+                                ?>
+                                <table border="1">
+                                    <tr>
+                                        <td>
                                             <?php
-                                            echo "$record[detail_name]";
+                                            
+                                             echo $record2[disease_list_name]."Ле".$record2[year];
                                             ?>
-                                        </th>
-                                        <?php
-                                    }
-                                    ?>
-                                    </thead>
-                                    <tbody>
-                                    </tbody>
+                                            
+                                            
+                                        </td>
+                                         <td>
+                                           
+                                            
+                                        </td>
+                                        
+                                    </tr>
+                                     <tr>
+                                        <td>
+                                            
+                                        </td>
+                                          <td>
+                                            
+                                        </td>
+                                        
+                                    </tr>
+                                    
                                 </table>
+                              <?php 
+                                }
+                                ?>
                             </div>
                         </div>
                     </div>
